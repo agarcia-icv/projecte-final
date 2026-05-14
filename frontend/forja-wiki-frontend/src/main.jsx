96c9bd71fc49b49268476cd04e99f9ecc8dbd7d0
Adria Garcia <adria_garciajorda@iescarlesvallbona.cat>
2026-05-14 19:06:54 +0200

    Estils separats per arxius

--- frontend/forja-wiki-frontend/src/main.jsx
+++ frontend/forja-wiki-frontend/src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.css"
+import "./styles/navbar.css";
+import "./styles/home.css";
+import "./styles/profile.css";
+import "./styles/adminusers.css";
+import "./styles/auth.css";
+import "./styles/postdetail.css";
+import "./styles/postlists.css";
+import "./styles/forge-buttons.css";
+import "./styles/createposts.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
